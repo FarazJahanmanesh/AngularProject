f8caf8432781287ca7ea42c573c7ff1d5fb6d838
faraz jahan manesh <farazebipax@gmail.com>
2023-09-24 05:31:33 -0700

    delete all old models

--- AngularProject.EndPoint.Api/Models/Response/GetListUserResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AngularProject.Src.EndPoint.Api.Models.Response
-{
-    public class GetListUserResponse
-    {
-        public string UserName { get; set; }
-        public string NationalCode { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
