bdeb4bdfb44b13a9d36439f191851acadc96ddec
faraz jahan manesh <farazebipax@gmail.com>
2023-09-24 07:37:50 -0700

    add two other hash algorithms

--- AngularProject.Src.Core.Application/Helpers/HashAlgorithms.cs
+++ AngularProject.Src.Core.Application/Helpers/HashAlgorithms.cs
@@ -9,18 +9,39 @@ namespace AngularProject.Src.Core.Application.Helpers
 {
     public static class HashAlgorithms
     {
-        public static string SHA1HashCode(this string f)
+        public static string SHA256HashCode(this string input)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(input));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+        public static string SHA512HashCode(this string input)
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+            using (var hash = System.Security.Cryptography.SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+                var hashedInputStringBuilder = new System.Text.StringBuilder(128);
+                foreach (var b in hashedInputBytes)
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                return hashedInputStringBuilder.ToString();
+            }
+        }
+        public static string SHA1HashCode(this string input)
         {
             var sha1 = new SHA1Managed();
-            var plaintextBytes = Encoding.UTF8.GetBytes(f);
+            var plaintextBytes = Encoding.UTF8.GetBytes(input);
             var hashBytes = sha1.ComputeHash(plaintextBytes);
-
             var sb = new StringBuilder();
             foreach (var hashByte in hashBytes)
             {
                 sb.AppendFormat("{0:x2}", hashByte);
             }
-
             var hashString = sb.ToString();
             return hashString;
         }
