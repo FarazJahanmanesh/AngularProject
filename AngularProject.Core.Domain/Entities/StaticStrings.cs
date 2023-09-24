ad8cb81d4959f7e3457e90982aa5ee846f7ab220
faraz jahan manesh <farazebipax@gmail.com>
2023-09-24 07:00:56 -0700

    add region to static strings

--- AngularProject.Core.Domain/Entities/StaticStrings.cs
+++ AngularProject.Core.Domain/Entities/StaticStrings.cs
@@ -8,18 +8,24 @@ namespace AngularProject.Src.Core.Domain.Entities
 {
     public static class StaticStrings
     {
+        #region Successfuly
         public const string Update_User_Successfuly = "تغییر کاربر موفقیت آمیز بود";
         public const string Delete_User_Successfuly = "حذف کاربر موفقیت آمیز بود";
         public const string Create_User_Successfuly = "اضافه کردن کاربر موفقیت آمیز بود";
         public const string Get_User_By_Id_Successfuly = "اطلاعات کاربر با موفقیت واکشی شد";
         public const string Get_List_User_Successfuly = "اطلاعات کاربران با موفقیت واکشی شد";
+        #endregion
 
+        #region Other
         public const string Problem_In_Calling_Service = "فراخوانی سرویس با مشکل مواجه شده است";
+        #endregion
 
+        #region Not_Successfuly
         public const string Update_User_Not_Successfuly = "تغییر کاربر موفقیت آمیز نبود";
         public const string Delete_User_Not_Successfuly = "حذف کاربر موفقیت آمیز نبود";
         public const string Create_User_Not_Successfuly = "اضافه کردن کاربر موفقیت آمیز نبود";
         public const string Get_User_By_Id_Not_Successfuly = "اطلاعات کاربر واکشی نشد";
         public const string Get_List_User_Not_Successfuly = "اطلاعات کاربران واکشی نشد";
+        #endregion
     }
 }
