22b74902494508b6b1210b291fbf0ae6e9e64fea
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2023-09-25 14:38:57 +0330

    add some default to ocelot

--- AngularProject.Src.Api.ApiGetWay.OcelotGetWay/Program.cs
+++ AngularProject.Src.Api.ApiGetWay.OcelotGetWay/Program.cs
@@ -1,6 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+string ocelotName = "";
+if (builder.Environment.IsDevelopment())
+{
+    ocelotName = "ocelot.development.json";
+}
+else
+{
+    ocelotName = "ocelot.production.json";
+}
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCorsPolicy", builder => { builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
+});
 var app = builder.Build();
-
+app.UseCors("MyCorsPolicy");
 app.MapGet("/", () => "Hello World!");
-
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+await app.UseOcelot();
 app.Run();
